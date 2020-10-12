3f5f01adee6f2f688472a59a854df852725a959b
Pedro Lopes <pedrolopes@ipiranga.ipiranga>
2020-10-11 21:59:44 -0300

    Feat: Add account entity

---
+++ pkg/adding/account.go
@@ -0,0 +1,12 @@
+package adding
+
+import "time"
+
+// Account is the representation of an account to be added
+type Account struct {
+	Name      string `json:"name"`
+	CPF       string `json:"cpf"`
+	Secret    string `json:"secret"`
+	Balance   string `json:"balance"`
+	CreatedAt time.Time
+}
