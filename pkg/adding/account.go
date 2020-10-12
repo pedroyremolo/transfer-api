18c6b3339e7c3730d142d7f6b70d6d9668e4718e
Pedro Lopes <pedrolopes@ipiranga.ipiranga>
2020-10-11 22:55:08 -0300

    Fix: Account entity Balance type

--- pkg/adding/account.go
+++ pkg/adding/account.go
@@ -4,9 +4,9 @@ import "time"
 
 // Account is the representation of an account to be added
 type Account struct {
-	Name      string `json:"name"`
-	CPF       string `json:"cpf"`
-	Secret    string `json:"secret"`
-	Balance   string `json:"balance"`
+	Name      string  `json:"name"`
+	CPF       string  `json:"cpf"`
+	Secret    string  `json:"secret"`
+	Balance   float64 `json:"balance"`
 	CreatedAt time.Time
 }
