af7ac60b210243fbe96170855fb1e588907941ab
Pedro Lopes <pedrolopes@ipiranga.ipiranga>
2020-10-15 11:43:10 -0300

    Feat: Add server building logic to receive POST /accounts requests

---
+++ cmd/transfer-server/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/pedroyremolo/transfer-api/pkg/adding"
+	"github.com/pedroyremolo/transfer-api/pkg/http/rest"
+	"github.com/pedroyremolo/transfer-api/pkg/storage/mongodb"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var adder adding.Service
+	var port int
+
+	storage, err := mongodb.NewStorageFromEnv()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	dbCtx := context.Background()
+	storage.Connect(dbCtx)
+	defer storage.Disconnect(dbCtx)
+
+	adder = adding.NewService(storage)
+
+	handler := rest.Handler(adder)
+	port, err = strconv.Atoi(os.Getenv("APP_PORT"))
+	portStr := fmt.Sprintf(":%d", port)
+	fmt.Printf("Starting server at port %s", portStr)
+	log.Fatal(http.ListenAndServe(portStr, handler))
+}
