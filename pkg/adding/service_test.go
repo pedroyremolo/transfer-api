88fd7641c1deeff33032aeb31003f6074ea519ef
Pedro Lopes <pedrolopes@ipiranga.ipiranga>
2020-10-24 13:20:22 -0300

    Test: Improve adding account testing by including err cases

--- pkg/adding/service_test.go
+++ pkg/adding/service_test.go
@@ -2,6 +2,7 @@ package adding
 
 import (
 	"context"
+	"errors"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"testing"
 	"time"
@@ -9,41 +10,80 @@ import (
 
 var zeroTime = time.Time{}
 
-func TestAddAccount(t *testing.T) {
-
-	a := Account{
-		Name:      "Gopher",
-		CPF:       "11111111030",
-		Secret:    []byte("g0rul&zz"),
-		Balance:   8000.00,
-		CreatedAt: time.Time{},
+func TestService_AddAccount(t *testing.T) {
+	tt := []struct {
+		name        string
+		account     Account
+		expectedErr error
+	}{
+		{
+			name: "When successfully adds",
+			account: Account{
+				Name:      "Gopher",
+				CPF:       "11111111030",
+				Secret:    []byte("g0rul&zz"),
+				Balance:   8000.00,
+				CreatedAt: time.Time{},
+			},
+		},
+		{
+			name: "When an error occurs",
+			account: Account{
+				Name:      "Gopher",
+				CPF:       "11111111030",
+				Secret:    []byte("g0rul&zz"),
+				Balance:   8000.00,
+				CreatedAt: time.Time{},
+			},
+			expectedErr: errors.New("foo"),
+		},
 	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			mockRepo := new(mockStorage)
+			mockRepo.expectedErr = tc.expectedErr
+			s := NewService(mockRepo)
+			ctx := context.TODO()
+			id, err := s.AddAccount(ctx, tc.account)
 
-	mockRepo := new(mockStorage)
-	s := NewService(mockRepo)
-	ctx := context.TODO()
-	id, err := s.AddAccount(ctx, a)
+			if err != nil && tc.expectedErr == nil {
+				t.Errorf("Expected nil, got %s", err)
+			}
 
-	if err != nil {
-		t.Errorf("Expected nil, got %s", err)
-	}
+			if mockRepo.a.CreatedAt == zeroTime && tc.expectedErr == nil {
+				t.Errorf("Expected account with CreatedAt near %s, got %s", time.Now().UTC(), mockRepo.a.CreatedAt)
+			}
 
-	if mockRepo.a.CreatedAt == zeroTime {
-		t.Errorf("Expected account with CreatedAt near %s, got %s", time.Now().UTC(), mockRepo.a.CreatedAt)
-	}
-
-	if id != mockRepo.oid.Hex() {
-		t.Errorf("Expected id %s, got %s", id, mockRepo.oid.Hex())
+			if id != mockRepo.oid.Hex() && tc.expectedErr == nil {
+				t.Errorf("Expected id %s, got %s", id, mockRepo.oid.Hex())
+			}
+		})
 	}
 }
 
 type mockStorage struct {
-	a   Account
-	oid primitive.ObjectID
+	a           Account
+	t           Transfer
+	expectedErr error
+	oid         primitive.ObjectID
 }
 
 func (m *mockStorage) AddAccount(_ context.Context, account Account) (string, error) {
 	m.a = account
+	return m.idOrErr()
+}
+
+func (m *mockStorage) AddTransfer(_ context.Context, transfer Transfer) (string, error) {
+	m.t = transfer
+	return m.idOrErr()
+}
+
+func (m *mockStorage) idOrErr() (string, error) {
 	m.oid = primitive.NewObjectID()
+
+	if m.expectedErr != nil {
+		return "", m.expectedErr
+	}
+
 	return m.oid.Hex(), nil
 }
