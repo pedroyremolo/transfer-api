160d1e2f41c42a9c5a4598537b6e20b2bad00afd
Pedro Lopes <pedrolopes@ipiranga.ipiranga>
2020-10-24 23:14:26 -0300

    Refactor: Remove unused flags from adding transfer model

--- pkg/adding/transfer.go
+++ pkg/adding/transfer.go
@@ -3,8 +3,8 @@ package adding
 import "time"
 
 type Transfer struct {
-	OriginAccountID      string
-	DestinationAccountID string
-	Amount               float64
+	OriginAccountID      string  `json:"account_origin_id"`
+	DestinationAccountID string  `json:"account_destination_id"`
+	Amount               float64 `json:"amount"`
 	CreatedAt            time.Time
 }
