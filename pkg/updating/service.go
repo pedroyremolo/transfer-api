a9b3965ce577754488e9e17d24c5bf99ae287439
Pedro Lopes <pedrolopes@ipiranga.ipiranga>
2020-10-25 21:17:27 -0300

    Feat: Adds primal logging to updating service

--- pkg/updating/service.go
+++ pkg/updating/service.go
@@ -1,6 +1,10 @@
 package updating
 
-import "context"
+import (
+	"context"
+	"github.com/pedroyremolo/transfer-api/pkg/log/lgr"
+	"github.com/sirupsen/logrus"
+)
 
 type Service interface {
 	UpdateAccounts(ctx context.Context, accounts ...Account) error
@@ -11,13 +15,18 @@ type Repository interface {
 }
 
 type service struct {
-	r Repository
+	r   Repository
+	log *logrus.Logger
 }
 
 func (s *service) UpdateAccounts(ctx context.Context, accounts ...Account) error {
-	return s.r.UpdateAccounts(ctx, accounts)
+	if err := s.r.UpdateAccounts(ctx, accounts); err != nil {
+		s.log.Errorf("Err %v when updating %v", err, accounts)
+		return err
+	}
+	return nil
 }
 
 func NewService(repository Repository) Service {
-	return &service{r: repository}
+	return &service{r: repository, log: lgr.NewDefaultLogger()}
 }
